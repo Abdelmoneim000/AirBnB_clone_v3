2df386dfb004c1cb142a657de798e5389a88e323
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-28 18:25:44 +0200

    fix small errors and making files executable

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """A module to handle the blueprints"""
 
-from api.v1.views.index import *
 from flask import Flask, Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
