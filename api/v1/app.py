c56b76d59d1eb64eda0abe26d6cefe510a873517
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-28 17:49:58 +0200

    fix small errors and making files executable

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """Creating the Route needed for the application"""
+from api.v1.views import app_views
 from flask import Flask
 from os import environ
 from models import storage
-from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def shutdown_session(exception=None):
-    """Shutdown the storage"""
+def close_storage(exception=None):
+    """Close the storage when the app context is torn down."""
     storage.close()
 
 if __name__ == "__main__":
-    HBNB_API_HOST = environ.get('HBNB_API_HOST')
-    HBNB_API_PORT = environ.get('HBNB_API_PORT')
-    app.run(host=HBNB_API_HOST or '0.0.0.0', port=HBNB_API_PORT or 5000, threaded=True)
+    HBNB_API_HOST = environ.get('HBNB_API_HOST', '0.0.0.0')
+    HBNB_API_PORT = int(environ.get('HBNB_API_PORT', 5000))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
