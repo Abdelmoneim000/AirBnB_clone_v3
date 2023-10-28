053923af9b0236dfdb94c9c9204a1b30242162ea
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-28 17:11:41 +0200

    finish task 4 creating the API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,5 +6,6 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    """define the status message"""
     response = {"status": "OK"}
     return response
