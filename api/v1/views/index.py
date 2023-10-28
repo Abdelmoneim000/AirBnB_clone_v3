70bc78cf209e4f3dacbba721b5865fbaee3f8cb9
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-28 18:38:37 +0200

    finish task 4 creates a new route for stats to count all objects in storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 A module to handle the status messages
 """
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -12,3 +13,16 @@ def get_status():
         "status": "OK"
         }
     return response
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_number_of_objects():
+    """A route to count the number of every object type"""
+    obj = {
+        "amenity": storage.count("amenity"),
+        "base_model": storage.count("baseModel"),
+        "city": storage.count("city"),
+        "place": storage.count("place"),
+        "review": storage.count("review"),
+    }
+    return obj
