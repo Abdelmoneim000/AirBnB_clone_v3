1931f334582405a766a513f6b4dbd5e1d6070e44
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-30 00:07:46 +0200

    unknown error output for the task 6 and 7

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,11 @@ from api.v1.views import app_views
 from flask import Flask,  jsonify
 from os import environ
 from models import storage
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_storage(exception=None):
