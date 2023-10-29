525ad94f42483d0f1be3b879e79d19dadb16d870
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-29 19:01:35 +0200

    add ignoring execute condition

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+"""
+A module to handle the status messages
+"""
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
 from models import storage
+from flask import jsonify
 
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """define the status message"""
+    response = {
+        "status": "OK"
+        }
+    return response
 
 
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
+@app_views.route('/stats', methods=['GET'])
+def get_number_of_objects():
+    """A route to count the number of every object type"""
+    obj = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(obj)
 
 if __name__ == "__main__":
     pass
