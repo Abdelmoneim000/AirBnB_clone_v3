f5dd89d4737c1a360275301965e031f1f367ac02
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-31 13:24:03 +0200

    fix port error

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """Creating the Route needed for the application"""
 from api.v1.views import app_views
 from flask import Flask,  jsonify
-from os import environ
+from os import getenv
 from models import storage
 from flask_cors import CORS
 
@@ -26,12 +26,12 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    if environ.getenv("HBNB_API_HOST") is None:
+    if getenv("HBNB_API_HOST") is None:
         HBNB_API_HOST = '0.0.0.0'
     else:
-        HBNB_API_HOST = environ.getenv("HBNB_API_HOST")
-    if environ.getenv("HBNB_API_PORT") is None:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
         HBNB_API_PORT = 5000
     else:
-        HBNB_API_PORT = int(environ.getenv("HBNB_API_PORT"))
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
